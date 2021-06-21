1293a8d7ee2870ab8da49f5590023ee1565fe89e
binh.buidang <dangbinh4869@gmail.com>
2021-06-21 10:13:32 +0700

    add: display fps.

---
+++ Assets/Scripts/FPSDisplay.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class FPSDisplay : MonoBehaviour
+{
+	private float _deltaTime;
+
+	private void Update()
+	{
+		_deltaTime += (Time.unscaledDeltaTime - _deltaTime) * 0.1f;
+	}
+
+	private void OnGUI()
+	{
+		int w = Screen.width, h = Screen.height;
+
+		var style = new GUIStyle();
+
+		var rect = new Rect(0, 0, w, h * 2f / 100);
+		style.alignment = TextAnchor.UpperRight;
+		style.fontSize = h * 2 / 100;
+		style.normal.textColor = Color.green;
+		var msec = _deltaTime * 1000.0f;
+		var fps = 1.0f / _deltaTime;
+		var text = $"{msec:0.0} ms ({fps:0.} fps)";
+		GUI.Label(rect, text, style);
+	}
+}
