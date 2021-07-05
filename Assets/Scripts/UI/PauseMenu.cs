d5fbf40fde9416c2ff3a78180148e0fed051354f
binh.buidang <dangbinh4869@gmail.com>
2021-07-05 22:47:13 +0700

    update: refactor code.

--- Assets/Scripts/UI/PauseMenu.cs
+++ Assets/Scripts/UI/PauseMenu.cs
@@ -65,13 +65,14 @@ namespace UI
 
         public void RequestAdsReward()
         {
-            //AdsManager.Instance.ShowReward();
-            //AdsManager.Instance.OnEarnedReward += () =>
-            //{
-            //    animator.Play("RewardAd");
-            //    GameStats.Instance.Coin += 2000;
-            //    coinTmp.SetText($"Coin: {GameStats.Instance.Coin}");
-            //};
+            AdsManager.Instance.ShowReward();
+            AdsManager.Instance.OnEarnedReward += () =>
+            {
+                animator.Play("RewardAd");
+                GameStats.Instance.Coin += 2000;
+                coinTmp.SetText($"Coin: {GameStats.Instance.Coin}");
+                GameStats.Instance.SaveStatsToFile();
+            };
         }
     }
 }
